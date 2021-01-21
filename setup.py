26bb700b4625242c536151867c0547e219b5c395
Richard Tsai <richard9404@gmail.com>
2021-01-21 09:34:27 +0800

    update dependencies

--- setup.py
+++ setup.py
@@ -12,9 +12,9 @@ setup(
     },
     python_requires='>=3.7',
     install_requires=[
-        'cos-python-sdk-v5>=1.7.4',
-        'cryptography>=2.8',
-        'lz4>=2.2.1',
-        'toml>=0.10.0',
+        'cos-python-sdk-v5>=1.7.4,<1.9.0',
+        'cryptography>=3.3.1',
+        'lz4>=3.1.1',
+        'toml>=0.10.2',
     ],
 )
